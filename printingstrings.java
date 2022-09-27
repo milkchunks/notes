19ce2b680eb08d7b4da94795abbc7a0ce5b12390
Allison Bowe <allisonhbowe@gmail.com>
2022-09-27 23:46:30 +0000

    Upload New File

---
+++ printingstrings.java
@@ -0,0 +1,14 @@
+public class printingstrings {
+    public static void main(String[] args) {
+        String var1 = "Hello "; //this makes a string holding the word 'hello', and you can print this string
+        String var2 = "World";
+        System.out.println(var1 + var2);
+
+        String number1 = "number";
+        System.out.println(1 + number1 + 2);
+        System.out.println(1 + " numberone " + 2);
+
+        int sugar = 500;
+        System.out.println("There are " + sugar + "mg of sugar in a can of Coca-Cola");
+    }
+}
