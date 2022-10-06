7055eaa8a6fb398e7a6eb931778eb1bf4221ad7a
Allison Bowe <allisonhbowe@gmail.com>
2022-10-06 23:56:25 +0000

    Replace CalculatorHW.java

--- CalculatorHW.java
+++ CalculatorHW.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 public class CalculatorHW {
     //this calculator is more user friendly and doesn't require logging in
     public static void main(String[] args) {
-        System.out.println("Calculator");
-        System.out.println("Input a number, operator, and another number all on different lines");
-        System.out.println("Use ^ to represent an exponent");
-        System.out.println(doMath());
+            System.out.println("Calculator");
+            System.out.println("Input a number, operator, and another number all on different lines");
+            System.out.println("Use ^ to represent an exponent");
+            Double integer = doMath(); //i don't know why this works but it does
+            if (integer != -0.5878374859743589) {
+                System.out.println(doMath());
+            } else {
+                System.out.println(integer);
+                System.out.println("Error: invalid operator");
+            }
     }
     public static double readNumberOne() {
         Scanner numberOneReader = new Scanner(System.in);
@@ -24,23 +30,33 @@ public class CalculatorHW {
     }
 
     public static double doMath() {
-        double numberOne = readNumberOne();
-        String operator = readOperator();
-        double numberTwo = readNumberTwo();
-
-        if (operator.equals("+")) {
-            return (numberOne + numberTwo);
-        } else if (operator.equals("-")) {
-            return (numberOne - numberTwo);
-        } else if (operator.equals("*")) {
-            return (numberOne * numberTwo);
-        } else if (operator.equals("/")) {
-            return (numberOne / numberTwo);
-        } else if (operator.equals("%")) {
-            return (numberOne % numberTwo);
-        } else if (operator.equals("^")) {
-            return (Math.pow(numberOne, numberTwo));
-        }
-        return (3);
+            double numberOne = readNumberOne();
+            String operator = readOperator();
+            double numberTwo = readNumberTwo();
+            double integer;
+            switch (operator) {
+                case "+":
+                    integer = numberOne + numberTwo;
+                    break;
+                case "-":
+                    integer = numberOne - numberTwo;
+                    break;
+                case "/":
+                    integer = numberOne / numberTwo;
+                    break;
+                case "*":
+                    integer = numberOne * numberTwo;
+                    break;
+                case "%":
+                    integer = numberOne % numberTwo;
+                    break;
+                case "^":
+                    integer = Math.pow(numberOne, numberTwo);
+                    break;
+                default:
+                    integer = -0.5878374859743589;
+                    break;
+            }
+            return (integer);
     }
 }
