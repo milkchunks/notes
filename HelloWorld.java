752ade6908153faaaa961b319d32703a6066c96a
Allison Bowe <allisonhbowe@gmail.com>
2022-11-10 23:11:04 +0000

    Upload New File

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package Exercises;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /*
+        println prints numbers too
+        quotes mean text, quotes mean numbers (no quotes can do math)
+        multiline comments: /* and //
+        use these like all the time
+        */
+        int x = 3;
+        System.out.println(3*x+2);
+        //woah 'advanced' printing
+    }
+}
