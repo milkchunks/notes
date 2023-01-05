557f3d0b33a130982177576537438d4414169cf5
Allison Bowe <cattim569@gmail.com>
2023-01-05 00:53:37 +0000

    Replace ValCalc.java

--- ValCalc.java
+++ ValCalc.java
@@ -9,9 +9,9 @@ public class ValCalc {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What battlepass level are you on?");
+        System.out.println("What battle pass level are you on?");
         lvl = scanner.nextInt();
-        System.out.println("Do you have the premium battlepass track? (y/n)");
+        System.out.println("Do you have the premium battle pass track? (y/n)");
         String yn = scanner.next();
         if (yn.equalsIgnoreCase("y")) {
             xp = 1030;
@@ -20,7 +20,6 @@ public class ValCalc {
         } else {
             System.out.println("Error: Invalid answer");
         }
-        System.out.println(calcLvlXp());
         //1k xp per game, so how many games until you meet lvl 50?
         double gameNum = calcLvlXp()/xp;
         double maxMinutes = gameNum * maxGameTime;
@@ -36,14 +35,24 @@ public class ValCalc {
         System.out.println("Max Time: " + list.get(0) + "h " + list.get(1) + "m, " + "Min Time: " + list.get(2) + "h " + list.get(3) + "m, " + "Avg Time: " + list.get(4) + "h " + list.get(5) + "m");
     }
     public static Integer calcLvlXp() {
-        Integer lvl2 = 50-lvl;
-        Integer x = 1250;
-        Integer y = null;
-        //calculates how much xp your level needs to be completed
-        for (int i=1; i<=lvl; i++) {
-            x = x + 750;
-            y = x;
+        Integer lvl2;
+        lvl2 = 50-lvl;
+        Integer x;
+        if (lvl == 1) {
+            x = 0;
+        } else {
+            x = 1250;
         }
+        Integer y;
+        //calculates how much xp you have earned to get to your current level
+        if (lvl != 1) {
+            for (int i = 1; i <= lvl; i++) {
+                x = x + 750;
+            }
+        } else {
+
+        }
+        y = x;
         //calculates how much xp you need for lvl 50
         for (int i=1; i<=lvl2; i++) {
             y = y + x;
