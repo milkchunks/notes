e011da14037770c57974707e1b150b4dbaa9f5aa
Allison Bowe <cattim569@gmail.com>
2023-02-15 19:08:39 +0000

    renamed class to fit file name & renamed variables to be more descriptive

--- PemdasCalculatorBeta.java
+++ PemdasCalculatorBeta.java
@@ -36,9 +36,9 @@ public class PemdasCalculator {
             int expoCount = Collections.frequency(all, "^");
             int multCount = Collections.frequency(all, "*") + Collections.frequency(all, "/") + Collections.frequency(all, "%");
             int addCount = Collections.frequency(all, "+") + Collections.frequency(all, "-");
-            String tempEnd = null;
+            String toAdd = null;
             int pos = 0;
-            int temp = 0;
+            int signCount = 0;
             boolean removed = false;
             int parPos = all.indexOf("(");
             if (parCount != 0) {
@@ -51,62 +51,62 @@ public class PemdasCalculator {
                         do {
                             switch (all.get(pos)) {
                                 case "^":
-                                    tempEnd = String.valueOf(Math.pow(Double.valueOf(all.get(pos - 1)), Double.valueOf(all.get(pos + 1))));
+                                    toAdd = String.valueOf(Math.pow(Double.valueOf(all.get(pos - 1)), Double.valueOf(all.get(pos + 1))));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     expoCount = expoCount - 1;
                                     break;
                                 case "*":
-                                    tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) * Double.valueOf(all.get(pos + 1)));
+                                    toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) * Double.valueOf(all.get(pos + 1)));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     multCount = multCount - 1;
                                     break;
                                 case "%":
-                                    tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) % Double.valueOf(all.get(pos + 1)));
+                                    toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) % Double.valueOf(all.get(pos + 1)));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     multCount = multCount - 1;
                                     break;
                                 case "/":
-                                    tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) / Double.valueOf(all.get(pos + 1)));
+                                    toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) / Double.valueOf(all.get(pos + 1)));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     multCount = multCount - 1;
                                     break;
                                 case "+":
-                                    tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) + Double.valueOf(all.get(pos + 1)));
+                                    toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) + Double.valueOf(all.get(pos + 1)));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     addCount = addCount - 1;
                                     break;
                                 case "-":
-                                    tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) - Double.valueOf(all.get(pos + 1)));
+                                    toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) - Double.valueOf(all.get(pos + 1)));
                                     all.remove(pos - 1);
                                     all.remove(pos);
                                     all.remove(pos - 1);
-                                    all.add(pos - 1, tempEnd);
-                                    temp++;
+                                    all.add(pos - 1, toAdd);
+                                    signCount++;
                                     removed = true;
                                     addCount = addCount - 1;
                                     break;
@@ -122,36 +122,36 @@ public class PemdasCalculator {
                     pos++;
                 }
                 //TODO this < is a bandaid, fix later
-            } while (temp < parCount);
+            } while (signCount < parCount);
             all.remove(parPos);
             all.remove(parPos + 1);
             }
             removed = false;
             pos = 0;
-            temp = 0;
+            signCount = 0;
             do {
                 if (all.size() == 1) {
                     return all.get(0);
                 }
                 removed = false;
                 if (all.get(pos).equals("^")) {
-                    tempEnd = String.valueOf(Math.pow(Double.valueOf(all.get(pos-1)), Double.valueOf(all.get(pos+1))));
+                    toAdd = String.valueOf(Math.pow(Double.valueOf(all.get(pos-1)), Double.valueOf(all.get(pos+1))));
                     all.remove(pos - 1);
                     all.remove(pos);
                     all.remove(pos - 1);
-                    all.add(pos - 1, tempEnd);
+                    all.add(pos - 1, toAdd);
                     removed = true;
                 }
                 if (!removed) {
                     pos++;
                 }
                 if (removed) {
-                    temp++;
+                    signCount++;
                 }
-            } while (temp != expoCount);
+            } while (signCount != expoCount);
             removed = false;
             pos = 0;
-            temp = 0;
+            signCount = 0;
             do {
                 removed = false;
                 if (all.size() == 1) {
@@ -160,13 +160,13 @@ public class PemdasCalculator {
                 if (all.get(pos).equals("/") || all.get(pos).equals("%") || all.get(pos).equals("*")) {
                 switch (all.get(pos)) {
                     case "*":
-                        tempEnd = String.valueOf(Double.valueOf(all.get(pos-1)) * Double.valueOf(all.get(pos+1)));
+                        toAdd = String.valueOf(Double.valueOf(all.get(pos-1)) * Double.valueOf(all.get(pos+1)));
                         break;
                     case "/":
-                        tempEnd = String.valueOf(Double.valueOf(all.get(pos-1)) / Double.valueOf(all.get(pos+1)));
+                        toAdd = String.valueOf(Double.valueOf(all.get(pos-1)) / Double.valueOf(all.get(pos+1)));
                         break;
                     case "%":
-                        tempEnd = String.valueOf(Double.valueOf(all.get(pos-1)) % Double.valueOf(all.get(pos+1)));
+                        toAdd = String.valueOf(Double.valueOf(all.get(pos-1)) % Double.valueOf(all.get(pos+1)));
                         break;
                     
                     default: 
@@ -175,19 +175,19 @@ public class PemdasCalculator {
                     all.remove(pos - 1);
                     all.remove(pos);
                     all.remove(pos - 1);
-                    all.add(pos - 1, tempEnd);
+                    all.add(pos - 1, toAdd);
                 removed = true;
                 }
                 if (!removed) {
                     pos++;
                 }
                 if (removed) {
-                    temp++;
+                    signCount++;
                 }
-            } while (temp != multCount);
+            } while (signCount != multCount);
             removed = false;
             pos = 0;
-            temp = 0;
+            signCount = 0;
             do {
                 if (all.size() == 1) {
                     return all.get(0);
@@ -196,10 +196,10 @@ public class PemdasCalculator {
                 if (all.get(pos).equals("-") || all.get(pos).equals("+")) {
                     switch (all.get(pos)) {
                         case "+":
-                            tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) + Double.valueOf(all.get(pos + 1)));
+                            toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) + Double.valueOf(all.get(pos + 1)));
                             break;
                         case "-":
-                            tempEnd = String.valueOf(Double.valueOf(all.get(pos - 1)) - Double.valueOf(all.get(pos + 1)));
+                            toAdd = String.valueOf(Double.valueOf(all.get(pos - 1)) - Double.valueOf(all.get(pos + 1)));
                         default:
                             break;
                     }
@@ -207,26 +207,26 @@ public class PemdasCalculator {
                     all.remove(pos - 1);
                     all.remove(pos);
                     all.remove(pos - 1);
-                    all.add(pos - 1, tempEnd);
+                    all.add(pos - 1, toAdd);
                 }
                 if (!removed) {
                     pos++;
                 }
                     if (removed) {
-                        temp++;
+                        signCount++;
                     }
                     removed = true;
-            } while (temp != addCount);
+            } while (signCount != addCount);
             end = all.get(0);
             return end;
-        } catch (IncompatibleClassChangeError e) {
-            //TODO change this error because its a temp error name
+        } catch (Exception e) {
             error = true;
             System.out.println("Error: Invalid Problem (Try again)");
         }
         return null;
     }
     public static ArrayList sorter(ArrayList<String> arr) {
+        //this method transforms [<any operator> ,-, x] (x being a number) into [<any operator>, -x]
         int pos = 0;
         int neg = 0;
         int negFrequency = Collections.frequency(arr, "-");
