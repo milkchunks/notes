49d0ed9a5ebb956ef5288fa32cb807f2ff5bf643
Allison Bowe <cattim569@gmail.com>
2023-03-17 18:44:39 +0000

    Replace W.java

--- W.java
+++ W.java
@@ -28,6 +28,7 @@ public class W {
                 }
             }
         } else {
+            buttonIndex++;
             if (buttonIndex == 30) {
                 buttonIndex = 0;
                 for (int i = 0; i <= 29; i++) {
@@ -35,9 +36,15 @@ public class W {
                 }
             }
             //if enter is pressed when buttonIndex is a multiple of 5
+            buttonIndex--;
+            System.out.println("String.valueOf(): " + (String.valueOf((Double.valueOf(buttonIndex)/5))));
+            System.out.println("buttonIndex: " + buttonIndex);
             if (String.valueOf((Double.valueOf(buttonIndex)/5)).contains(".0") && buttonIndex != 0) {
+                System.out.println("running changeColor()");
+                buttonIndex++;
                 changeColor();
             }
+            buttonIndex--;
         }
     };
     public static void main(String[] args) {
@@ -85,25 +92,59 @@ public class W {
         frame.add(backspace);
     }
     public static void changeColor() {
+        int pos = 0;
         //TODO broken, doesn't match letters
         ArrayList<String> texts = new ArrayList<>();
-        String[] words = {"polly", "jolly", "molly", "mucky"};
-        char[] word = words[(int) (Math.random() * 3) + 0].toCharArray();
-        for (int i=buttonIndex-5;i<=buttonIndex-1;i++) {
+        ArrayList<String> word = new ArrayList<>();
+        String[] words = {"polly", "jolly", "molly", "mucly"};
+        for (int i=0;i<=4;i++) {
+            word.add(String.valueOf(words[(int) Math.floor((Math.random() * 3) - 0)].charAt(i)));
+        }
+        for (int i=buttonIndex-6;i<=buttonIndex-2;i++) {
             JButton button = (JButton) frame.getContentPane().getComponent(i);
             texts.add(button.getText().toLowerCase());
         }
+        System.out.println("word: " + word);
         System.out.println("texts: " + texts);
-        for (int i=buttonIndex-5;i<=buttonIndex-1;i++) {
-            System.out.println("i: " + i);
-            System.out.println("word[i]: " + word[i]);
+        //texts is what the user guessed
+        //word is the correct answer
+        for (int i=buttonIndex-6;i<=buttonIndex-2;i++) {
             JButton button = (JButton) frame.getContentPane().getComponent(i);
-            System.out.println("button.getText(): " + button.getText().toLowerCase());
-            if (button.getText().toLowerCase().equals(word[i])) {
+            if (button.getText().toLowerCase().equals(word.get(pos))) {
                 button.setBackground(Color.GREEN);
-            } else if (texts.contains(word[i])) {
+            } else if (texts.contains(word.get(pos))) {
                 button.setBackground(Color.YELLOW);
+            } else {
+                button.setBackground(Color.GRAY);
             }
+            pos++;
         }
     }
+    /*
+    public static void changeColor() {
+        //TODO broken, doesn't match letters
+        ArrayList<String> texts = new ArrayList<>();
+        String[] words = {"polly", "jolly", "molly", "mucky"};
+        String word = words[(int) Math.floor((Math.random() * 3)-0)];
+        System.out.println("word: " + word);
+        String guess = "VRTFDMUDDYMUCKYMOLLYJOLLYPOLLY";
+        char[] guess2 = guess.toLowerCase().toCharArray();
+        //if the letter is in the right place, change to 1
+        //if the letter is in the word, change to 2
+        //if the letter isn't in the word, change to 3
+        int pos = 0;
+        for (int i=buttonIndex-4;i<=buttonIndex;i++) {
+            String guess2i = String.valueOf(guess2[i]);
+            if (guess2[i] == word.charAt(pos)) {
+                guess2[i] = '1';
+            } else if (word.contains(guess2i)) {
+                guess2[i] = '2';
+            } else {
+                guess2[i] = '3';
+            }
+            pos++;
+        }
+        System.out.println(guess2);
+    }
+     */
 }
