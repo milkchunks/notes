63291a93786c002295357e92103e5be0c4d9013f
Allison Bowe <cattim569@gmail.com>
2023-09-20 17:22:54 +0000

    Upload New File

---
+++ References.java
@@ -0,0 +1,18 @@
+public class References {
+    public static final String[] masterList = {"gastar", "comprender", "ir", "repetir", "dormir"};
+    public static final String[] arVerbs = {"gastar"};
+    public static final String[] erVerbs = {"comprender"};
+    public static final String[] irVerbs = {"repetir"};
+    public static final String[] irregs = {"repetir", "dormir", "ir"};
+    public static final String[] eToI = {"repetir"};
+    public static final String[] oToU = {"dormir"};
+    public static final String[] serIr = {"fui", "fuiste", "fue", "fuimos", "fuisteis", "fueron"};
+    public static final String[] arEndings = {"é", "aste", "ó", "amos", "asteis", "aron"};
+    public static final String[] erIrEndings = {"í", "iste", "ió", "imos", "isteis", "ieron"};
+    public static final String[] irregularEndings = {"e", "iste", "io", "imos", "isteis", "ieron"};
+    /*
+    ó
+    é
+    í
+     */
+}
